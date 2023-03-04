e25e56ce57a3c1731da5487915f207e9ea4239b2
HelloFlutterWorld <cta9427@gmail.com>
2023-03-04 18:09:07 +0900

    4.3 Sign Up Form code_challenge

--- lib/features/authentication/widgets/auth_button.dart
+++ lib/features/authentication/widgets/auth_button.dart
@@ -5,46 +5,78 @@ import 'package:tiktok_clone/constants/sizes.dart';
 class AuthButton extends StatelessWidget {
   final String text;
   final FaIcon icon;
-  final void Function(BuildContext)? onGesture;
+  final void Function(BuildContext context)? onTap;
 
   const AuthButton({
     super.key,
     required this.text,
     required this.icon,
-    this.onGesture,
+    this.onTap,
     //required this.onGesture,
   });
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () => onGesture!(context),
-      child: FractionallySizedBox(
-        widthFactor: 1,
-        child: Container(
-          padding: const EdgeInsets.all(14),
-          decoration: BoxDecoration(
-              border: Border.all(
-            color: Colors.grey.shade300,
-            width: Sizes.size1,
-          )),
-          child: Stack(
-            alignment: Alignment.center,
-            children: [
-              Align(
-                alignment: Alignment.centerLeft,
-                child: icon,
+    if (onTap != null) {
+      {
+        return GestureDetector(
+          onTap: () => onTap!(context),
+          child: FractionallySizedBox(
+            widthFactor: 1,
+            child: Container(
+              padding: const EdgeInsets.all(14),
+              decoration: BoxDecoration(
+                  border: Border.all(
+                color: Colors.grey.shade300,
+                width: Sizes.size1,
+              )),
+              child: Stack(
+                alignment: Alignment.center,
+                children: [
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: icon,
+                  ),
+                  Text(
+                    text,
+                    textAlign: TextAlign.center,
+                    style: const TextStyle(
+                      fontSize: Sizes.size16,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ],
               ),
-              Text(
-                text,
-                textAlign: TextAlign.center,
-                style: const TextStyle(
-                  fontSize: Sizes.size16,
-                  fontWeight: FontWeight.w600,
-                ),
-              ),
-            ],
+            ),
           ),
+        );
+      }
+    }
+    return FractionallySizedBox(
+      widthFactor: 1,
+      child: Container(
+        padding: const EdgeInsets.all(14),
+        decoration: BoxDecoration(
+            border: Border.all(
+          color: Colors.grey.shade300,
+          width: Sizes.size1,
+        )),
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            Align(
+              alignment: Alignment.centerLeft,
+              child: icon,
+            ),
+            Text(
+              text,
+              textAlign: TextAlign.center,
+              style: const TextStyle(
+                fontSize: Sizes.size16,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          ],
         ),
       ),
     );
