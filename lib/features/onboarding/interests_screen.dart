5a707c5ba417418d9cdff2e7f9752b4a28c009bc
HelloFlutterWorld <cta9427@gmail.com>
2023-03-17 11:43:09 +0900

    5.2 Scroll Animations

--- lib/features/onboarding/interests_screen.dart
+++ lib/features/onboarding/interests_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:tiktok_clone/constants/gaps.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
+import 'package:tiktok_clone/features/onboarding/widgets/interest_button.dart';
 
 const interests = [
   "Daily Life",
@@ -43,79 +44,99 @@ const interests = [
   "Home & Garden",
 ];
 
-class InterestsScreen extends StatelessWidget {
+class InterestsScreen extends StatefulWidget {
   const InterestsScreen({super.key});
+
+  @override
+  State<InterestsScreen> createState() => _InterestsScreenState();
+}
+
+class _InterestsScreenState extends State<InterestsScreen> {
+  final ScrollController _scrollController = ScrollController();
+
+  bool _showTitle = false;
+
+  void _onScroll() {
+    if (_scrollController.offset > 100) {
+      //offset의 값이 101,102,103이 되더라도 true로 변경하지 않는다.
+      if (_showTitle) return;
+      setState(() {
+        _showTitle = true;
+      });
+    } else {
+      setState(() {
+        _showTitle = false;
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController.addListener(_onScroll);
+
+    //offset는 위에서부터 얼마나 스크롤되었는지 알려준다.
+    //print(_scrollController.offset);
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: const Text("Choose your interests"),
+          //움직임에 따라 투명도를 조절하는 위젯이다.
+          title: AnimatedOpacity(
+            opacity: _showTitle ? 1 : 0,
+            duration: const Duration(milliseconds: 300),
+            child: const Text("Choose your interests"),
+          ),
         ),
-        body: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.only(
-              bottom: Sizes.size16,
-              left: Sizes.size24,
-              right: Sizes.size24,
-            ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Gaps.v32,
-                const Text(
-                  "Choose your interests",
-                  style: TextStyle(
-                    fontSize: Sizes.size40,
-                    fontWeight: FontWeight.bold,
+        body: Scrollbar(
+          controller: _scrollController,
+          child: SingleChildScrollView(
+            controller: _scrollController,
+            child: Padding(
+              padding: const EdgeInsets.only(
+                bottom: Sizes.size16,
+                left: Sizes.size24,
+                right: Sizes.size24,
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Gaps.v32,
+                  const Text(
+                    "Choose your interests",
+                    style: TextStyle(
+                      fontSize: Sizes.size40,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
-                ),
-                Gaps.v20,
-                const Text(
-                  "Get better video recommendations",
-                  style: TextStyle(
-                    fontSize: Sizes.size20,
+                  Gaps.v20,
+                  const Text(
+                    "Get better video recommendations",
+                    style: TextStyle(
+                      fontSize: Sizes.size20,
+                    ),
                   ),
-                ),
-                Gaps.v64,
-                //Wrap는 children을 옆으로 배치하되 한줄아 안들어가면
-                //줄바꿈을 한다.
-                Wrap(
-                  runSpacing: 15,
-                  spacing: 15,
-                  children: [
-                    for (var interest in interests)
-                      Container(
-                        padding: const EdgeInsets.symmetric(
-                          vertical: 16,
-                          horizontal: 24,
-                        ),
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(
-                            Sizes.size32,
-                          ),
-                          boxShadow: [
-                            BoxShadow(
-                              color: Colors.black.withOpacity(0.05),
-                              blurRadius: 5,
-                              spreadRadius: 5,
-                            ),
-                          ],
-                          border: Border.all(
-                            color: Colors.black.withOpacity(0.1),
-                          ),
-                        ),
-                        child: Text(
-                          interest,
-                          style: const TextStyle(
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ),
-                  ],
-                )
-              ],
+                  Gaps.v64,
+                  //Wrap는 children을 옆으로 배치하되 한줄아 안들어가면
+                  //줄바꿈을 한다.
+                  Wrap(
+                    runSpacing: 15,
+                    spacing: 15,
+                    children: [
+                      for (var interest in interests)
+                        InterestButton(interest: interest),
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
         ),
