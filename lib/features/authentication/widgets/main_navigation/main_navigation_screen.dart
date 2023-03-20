c0320c48613c0cddfeab5c04588c967362ac7e4d
HelloFlutterWorld <cta9427@gmail.com>
2023-03-20 10:45:02 +0900

    6.2 BottomNavigationBar

--- lib/features/authentication/widgets/main_navigation/main_navigation_screen.dart
+++ lib/features/authentication/widgets/main_navigation/main_navigation_screen.dart
@@ -1,10 +1,65 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-class MainNavigationScreen extends StatelessWidget {
+class MainNavigationScreen extends StatefulWidget {
   const MainNavigationScreen({super.key});
 
   @override
+  State<MainNavigationScreen> createState() => _MainNavigationScreenState();
+}
+
+class _MainNavigationScreenState extends State<MainNavigationScreen> {
+  int _selectedIndex = 0;
+
+  final screens = [
+    const Center(
+      child: Text("Home"),
+    ),
+    const Center(
+      child: Text("Search"),
+    ),
+    const Center(
+      child: Text("Search"),
+    ),
+    const Center(
+      child: Text("Search"),
+    ),
+    const Center(
+      child: Text("Search"),
+    ),
+  ];
+
+  void _onTap(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: screens[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        //아이템이 4개이상이면 자동으로 shifting,
+        type: BottomNavigationBarType.shifting,
+        onTap: _onTap,
+        currentIndex: _selectedIndex,
+        //selectedItemColor: Theme.of(context).primaryColor,
+        items: const [
+          BottomNavigationBarItem(
+            icon: FaIcon(FontAwesomeIcons.house),
+            label: "Home",
+            tooltip: "What are you? ",
+            backgroundColor: Colors.amber,
+          ),
+          BottomNavigationBarItem(
+            icon: FaIcon(FontAwesomeIcons.magnifyingGlass),
+            label: "Search ",
+            tooltip: "What are you? ",
+            backgroundColor: Colors.blue,
+          ),
+        ],
+      ),
+    );
   }
 }
