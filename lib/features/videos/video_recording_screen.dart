b81dd4f089784979dcfe03252c7cbd7eee84fb79
HelloFlutterWorld <cta9427@gmail.com>
2023-06-29 16:14:11 +0900

    19.2 CameraController_retry

--- lib/features/videos/video_recording_screen.dart
+++ lib/features/videos/video_recording_screen.dart
@@ -1,4 +1,8 @@
+import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:tiktok_clone/constants/gaps.dart';
+import 'package:tiktok_clone/constants/sizes.dart';
 
 class VideoRecordingScreen extends StatefulWidget {
   const VideoRecordingScreen({super.key});
@@ -8,11 +12,100 @@ class VideoRecordingScreen extends StatefulWidget {
 }
 
 class _VideoRecordingScreenState extends State<VideoRecordingScreen> {
+  bool _hasPermission = false;
+  bool _deniedPermission = false;
+
+  late final CameraController _cameraController;
+
+  Future<void> initCamera() async {
+    final cameras = await availableCameras();
+
+    if (cameras.isEmpty) {
+      return;
+    }
+
+    _cameraController = CameraController(
+      cameras[0],
+      ResolutionPreset.ultraHigh,
+    );
+
+    await _cameraController.initialize();
+  }
+
+  Future<void> initPermissions() async {
+    final cameraPermission = await Permission.camera.request();
+    final micPermission = await Permission.microphone.request();
+
+    final cameraDenied =
+        cameraPermission.isDenied || cameraPermission.isPermanentlyDenied;
+
+    final micDenied =
+        micPermission.isDenied || micPermission.isPermanentlyDenied;
+
+    if (!cameraDenied && !micDenied) {
+      _hasPermission = true;
+      await initCamera();
+      setState(() {});
+    } else {
+      _deniedPermission = true;
+      setState(() {});
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    initPermissions();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
-      body: Container(),
+      body: SizedBox(
+        width: MediaQuery.of(context).size.width,
+        child: !_hasPermission || !_cameraController.value.isInitialized
+            ? Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    !_deniedPermission
+                        ? "Initializing..."
+                        : "The camera and microphone permissions are required.",
+                    style: const TextStyle(
+                        color: Colors.white, fontSize: Sizes.size20),
+                    textAlign: TextAlign.center,
+                  ),
+                  Gaps.v20,
+                  if (!_deniedPermission)
+                    const CircularProgressIndicator.adaptive(),
+                  if (_deniedPermission) ...[
+                    Gaps.v96,
+                    GestureDetector(
+                      onTap: () async {
+                        await openAppSettings();
+                        initPermissions();
+                      },
+                      child: Container(
+                        padding: const EdgeInsets.all(Sizes.size8),
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.white),
+                        ),
+                        child: const Text(
+                          "Device Permission Settings",
+                          style: TextStyle(
+                            fontSize: Sizes.size20,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ]
+                ],
+              )
+            : CameraPreview(_cameraController),
+      ),
     );
   }
 }
