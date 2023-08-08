64e54837c7b9b41bf01194ed91449f035a285026
HelloFlutterWorld <cta9427@gmail.com>
2023-08-08 20:08:14 +0900

    27.4 likeVideo

--- lib/features/videos/repos/videos_repo.dart
+++ lib/features/videos/repos/videos_repo.dart
@@ -53,11 +53,14 @@ class VideoRepository {
   }
 
   Future<void> likeVideo(String videoId, String userId) async {
-    await _db.collection("likes").add({
-      "videoId": videoId,
-      "userId": userId,
-    });
-    //git test
+    final query = _db.collection("likes").doc("${videoId}000$userId");
+    final like = await query.get();
+
+    if (!like.exists) {
+      await query.set({
+        "createdAt": DateTime.now().millisecondsSinceEpoch,
+      });
+    }
   }
 }
 
